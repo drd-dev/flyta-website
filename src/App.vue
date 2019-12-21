1cd230a1ab2f1b11bb4b99b1f256a70e6cd18c9b
Dylan Dougherty <dyldougherty@gmail.com>
2019-12-21 11:10:56 -0500

    Initialcommit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img class="logo" src="./assets/logo.png" alt="">
+    <a  href="https://apps.apple.com/us/app/flyta/id1478797241?ls=1">
+      <img  class="appStore" src="./assets/appStore.svg" alt="">
+    </a>
+    <iframe class="video" width="375" height="315" src="https://www.youtube.com/embed/IJun4CwX6rM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <Support class="main"/>
+    <div class="footer">
+      <p>2019 Dylan Dougherty</p>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Support from './components/support.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Support,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import './style.scss';
+
+html{
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  background: #202020;
 }
+
+
+.appStore{
+    width: 35%;
+}
+
+.video{
+  padding-top: 80px;
+  display:block;
+  margin: 0 auto;
+
+}
+
+a{
+  padding-top: 40px;
+  margin: 0;
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+}
+
+
+.app{
+  width: 100%;
+}
+
+.logo{
+  padding-top: 100px;
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+  width: 70%;
+}
+
+
+
+#app {
+  height: 100%;
+  width: 100%;
+}
+
+.Support{
+  height: 100%;
+  height: 100%;
+  width: 100%;
+
+}
+.footer {
+  padding-top: 100px;
+  font-family: 'optician-sans';
+  width: 100%;
+  color: white;
+  text-align: left;
+
+  p{
+    color: $primary;
+    padding-left: 10%;
+  }
+}
+
 </style>
---
+++ src/components/support.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="support">
+    <div class="header2 light">
+      Support
+    </div>  
+    <div class="contact bodyText">
+      <p>For any questions or comments, please email us at </p> 
+      <a class="primary" href="mailto: support@flytagame.com">support@flytagame.com</a>
+      <p>we will try to respond as quickly as possible.</p>
+
+
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Support',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@import '../style.scss';
+
+
+  .support{
+    color: white;
+    text-align: center;
+    margin: 0 auto;
+    padding-top: 200px;
+    font-family: 'Optician-Sans';
+  }
+
+  .contact{
+  }
+
+</style>
